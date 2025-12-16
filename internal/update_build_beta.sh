edb420451d6b103b1d17a8ee550d0cb7fc2abb0d
xxcosita3czxx <kokosove18@gmail.com>
2025-12-16 19:33:42 +0100

    cron job setup

--- internal/update_build_beta.sh
+++ internal/update_build_beta.sh
@@ -3,6 +3,6 @@
 #./update_build_beta.sh <branch>
 
 cd /var/HoloISO_builder || exit 1
-./build.sh --output /var/www/holoiso --branch $1
+/var/HoloISO_builder/build.sh --output /var/www/holoiso --branch $1
 docker kill $(docker ps -q --filter ancestor=holoiso-build)
 docker rm $(docker ps -a -q --filter ancestor=holoiso-build)

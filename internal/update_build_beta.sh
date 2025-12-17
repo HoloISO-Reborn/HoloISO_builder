473bcb5987981e38d471c4421d101f6f21adcd19
xxcosita3czxx <kokosove18@gmail.com>
2025-12-17 15:22:44 +0100

    default the beta

--- internal/update_build_beta.sh
+++ internal/update_build_beta.sh
@@ -6,6 +6,6 @@ cd /var/HoloISO_builder || exit 1
 git stash
 git pull
 chmod +x /var/HoloISO_builder/build.sh /var/HoloISO_builder/internal/update_build_beta.sh
-/var/HoloISO_builder/build.sh --output /var/www/holoiso --branch $1
+/var/HoloISO_builder/build.sh --output /var/www/holoiso --branch beta
 docker kill $(docker ps -q --filter ancestor=holoiso-build)
 docker rm $(docker ps -a -q --filter ancestor=holoiso-build)

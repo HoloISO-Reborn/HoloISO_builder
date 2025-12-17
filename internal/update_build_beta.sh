926525a54d9681265d4d2f4072f517e39d03f50d
xxcosita3czxx <kokosove18@gmail.com>
2025-12-17 14:51:16 +0100

    pulling before building

--- internal/update_build_beta.sh
+++ internal/update_build_beta.sh
@@ -3,6 +3,9 @@
 #./update_build_beta.sh <branch>
 
 cd /var/HoloISO_builder || exit 1
+git stash
+git pull
+chmod +x /var/HoloISO_builder/build.sh /var/HoloISO_builder/internal/update_build_beta.sh
 /var/HoloISO_builder/build.sh --output /var/www/holoiso --branch $1
 docker kill $(docker ps -q --filter ancestor=holoiso-build)
 docker rm $(docker ps -a -q --filter ancestor=holoiso-build)
